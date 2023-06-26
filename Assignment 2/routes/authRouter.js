92ea04aab146403a8a1ee76f5b931f73e2c0f4f8
ihasnainbhutta <ihasnainbhutta@gmail.com>
2023-06-26 14:22:38 +0500

    minor changes

--- Assignment 2/routes/authRouter.js
+++ Assignment 2/routes/authRouter.js
@@ -4,7 +4,7 @@ const postController = require("../controllers/authController");
 const jwtMiddleware = require("../middlewares/auth");
 
 router.post("/user-register", postController.userRegister);
-router.post("/user-login", jwtMiddleware, postController.userLogin);
+router.post("/user-login", postController.userLogin);
 router.put("/user-update/:id", postController.updateUser);
 router.delete("/delete-user/:id", postController.userDelete);
 
